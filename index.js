6470b9916c2ce5b48b6f765b15a311346bc0a94f
root <root@DESKTOP-6I3RBRC>
2023-06-16 20:51:42 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//findMatching()
+//returns all drivers that match the passed in name
+//returns matching drivers if case does not match but letters do
+//returns an empty array if there is no match
+
+function findMatching(drivers, driverName){
+  return drivers.filter(drivers => drivers.toLowerCase()
+=== driverName.toLowerCase())  
+}
+
+function  fuzzyMatch(drivers, driverName){
+    return drivers.filter(drivers => drivers.charAt(0)=== driverName.charAt(0));
+  }
+
+ 
+
+function  matchName(drivers, driverName){
+const match = drivers.filter(drivers => drivers.name === driverName);
+return match;
+}
+
+
+
+
